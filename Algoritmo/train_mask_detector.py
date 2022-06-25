9c04b91bfa174dab6989d75902a02ef4d36420dd
murilofantozzi <42852328+murilofantozzi@users.noreply.github.com>
2022-06-25 17:38:52 -0300

    Dependencias

--- Algoritmo/train_mask_detector.py
+++ Algoritmo/train_mask_detector.py
@@ -26,12 +26,15 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+
+# DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+DIRECTORY = r"C:\Users\User\Desktop\Faculdade\SI\SIA\Algoritmo\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # pegue a lista de imagens em nosso diretório de conjunto de dados e inicialize
-#a lista de dados (ou seja, imagens) e imagens de classe
-print("[INFO] loading images...")
+# a lista de dados (ou seja, imagens) e imagens de classe
+print("[INFO] carregando imagens...")
+
 
 data = []
 labels = []
@@ -39,13 +42,13 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
+        img_path = os.path.join(path, img)
+        image = load_img(img_path, target_size=(224, 224)) 
+        image = img_to_array(image)
+        image = preprocess_input(image)
 
-    	data.append(image)
-    	labels.append(category)
+        data.append(image)
+        labels.append(category)
 
 # execute a codificação one-hot nos rótulos
 lb = LabelBinarizer()
@@ -56,22 +59,22 @@ data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+                                                  test_size=0.20, stratify=labels, random_state=42)
 
 # construir o gerador de imagem de treinamento para aumento de dados
 aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
+# carregar a rede MobileNetV2, garantindo que os conjuntos de camadas FC principais sejam
 # left off
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+                        input_tensor=Input(shape=(224, 224, 3)))
 
 # construir a cabeça do modelo que será colocada em cima do
 # o modelo básico
@@ -89,25 +92,25 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 # faz um loop sobre todas as camadas no modelo base e as congela para que elas
 # *não* ser atualizado durante o primeiro processo de treinamento
 for layer in baseModel.layers:
-	layer.trainable = False
+    layer.trainable = False
 
 # compilar nosso modelo
-print("[INFO] compiling model...")
+print("[INFO] compilando modelo...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # treinar o chefe da rede
-print("[INFO] training head...")
+print("[INFO] cabeça de treinamento...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
 
 # fazer previsões sobre o conjunto de testes
-print("[INFO] evaluating network...")
+print("[INFO] avaliando rede...")
 predIdxs = model.predict(testX, batch_size=BS)
 
 # para cada imagem no conjunto de teste, precisamos encontrar o índice do
@@ -116,10 +119,10 @@ predIdxs = np.argmax(predIdxs, axis=1)
 
 # mostra um relatório de classificação bem formatado
 print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
+                            target_names=lb.classes_))
 
 # serializar o modelo para o disco
-print("[INFO] saving mask detector model...")
+print("[INFO] salvando o modelo do detector de máscara...")
 model.save("mask_detector.model", save_format="h5")
 
 # plota a perda e a precisão do treinamento
--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 
 ## Proposta de uso da inteligência artificial no combate a COVID-19
 
+### Dependencias
+
+tensorflow>=1.15.2
+keras==2.3.1
+imutils==0.5.3
+numpy==1.18.2
+opencv-python==4.2.0.*
+matplotlib==3.2.1
+scipy==1.4.1
+
 ### Features
 
 - [x] Base de dados
