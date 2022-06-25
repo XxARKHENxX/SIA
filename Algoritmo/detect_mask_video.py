eed20c4590cc5ae3516e8e4eefa3548119360652
murilofantozzi <42852328+murilofantozzi@users.noreply.github.com>
2022-06-25 12:28:55 -0300

    Treianando o algoritmo

--- Algoritmo/detect_mask_video.py
+++ Algoritmo/detect_mask_video.py
@@ -10,119 +10,119 @@ import cv2
 import os
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
+	# pegue as dimensões do quadro e construa um blob
+	# a partir dele
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
 		(104.0, 177.0, 123.0))
 
-	# pass the blob through the network and obtain the face detections
+	# passe o blob pela rede e obtenha as detecções de rosto
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
 	print(detections.shape)
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
+	# inicialize uma lista de rostos, suas localizações correspondentes,
+	# a lista de previsões de nossa rede de máscaras faciais
 	faces = []
 	locs = []
 	preds = []
 
-	# loop over the detections
+	# loop sobre as detecções
 	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
+		# extrair a confiança (ou seja, probabilidade) associada a
+		# a detecção
 		confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
+		# filtre detecções fracas, garantindo que a confiança seja
+		# maior do que a confiança mínima
 		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
+			# calcule as coordenadas (x, y) da caixa delimitadora para
+			# o objeto
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
+			# garantir que as caixas delimitadoras estejam dentro das dimensões de
 			# the frame
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
+			# extraia o ROI da face, converta-o de BGR para canal RGB
+			# pedido, redimensione-o para 224x224 e pré-processe-o
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (224, 224))
 			face = img_to_array(face)
 			face = preprocess_input(face)
 
-			# add the face and bounding boxes to their respective
+			# adicione a face e as caixas delimitadoras aos seus respectivos
 			# lists
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
+	# só faça previsões se pelo menos um rosto for detectado
 	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
+		# para inferência mais rápida, faremos previsões em lote em *todos*
+		# rostos ao mesmo tempo, em vez de previsões um a um
+		# acima `for` loop
 		faces = np.array(faces, dtype="float32")
 		preds = maskNet.predict(faces, batch_size=32)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
+	# retornar uma tupla de 2 dos locais das faces e seus correspondentes
+	# Localizações
 	return (locs, preds)
 
-# load our serialized face detector model from disk
+# carregue nosso modelo de detector de rosto serializado do disco
 prototxtPath = r"face_detector\deploy.prototxt"
 weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
+# carregue o modelo do detector de máscara facial do disco
 maskNet = load_model("mask_detector.model")
 
-# initialize the video stream
+# inicializar o fluxo de vídeo
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 
-# loop over the frames from the video stream
+# loop sobre os quadros do fluxo de vídeo
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# pegue o quadro do fluxo de vídeo encadeado e redimensione-o
+	# ter uma largura máxima de 400 pixels
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
+	# detectar rostos no quadro e determinar se eles estão usando um
+	# máscara facial ou não
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-	# loop over the detected face locations and their corresponding
+	# loop sobre os locais de rosto detectados e seus correspondentes
 	# locations
 	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
+		# descompacte a caixa delimitadora e as previsões
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
+		# determine o rótulo e a cor da classe que usaremos para desenhar
+		# a caixa delimitadora e o texto
 		label = "Mask" if mask > withoutMask else "No Mask"
 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-		# include the probability in the label
+		# inclua a probabilidade no rótulo
 		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-		# display the label and bounding box rectangle on the output
+		# exibir o rótulo e o retângulo da caixa delimitadora na saída
 		# frame
 		cv2.putText(frame, label, (startX, startY - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-	# show the output frame
+	# mostrar o quadro de saída
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
+	# se a tecla `q` foi pressionada, saia do loop
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
+# fazer um pouco de limpeza
 cv2.destroyAllWindows()
 vs.stop()
--- Algoritmo/train_mask_detector.py
+++ Algoritmo/train_mask_detector.py
@@ -20,8 +20,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
+# inicializar a taxa de aprendizado inicial, número de épocas para treinar,
+# e tamanho do lote
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
@@ -29,8 +29,8 @@ BS = 32
 DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
+# pegue a lista de imagens em nosso diretório de conjunto de dados e inicialize
+#a lista de dados (ou seja, imagens) e imagens de classe
 print("[INFO] loading images...")
 
 data = []
@@ -47,7 +47,7 @@ for category in CATEGORIES:
     	data.append(image)
     	labels.append(category)
 
-# perform one-hot encoding on the labels
+# execute a codificação one-hot nos rótulos
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
@@ -58,7 +58,7 @@ labels = np.array(labels)
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
 	test_size=0.20, stratify=labels, random_state=42)
 
-# construct the training image generator for data augmentation
+# construir o gerador de imagem de treinamento para aumento de dados
 aug = ImageDataGenerator(
 	rotation_range=20,
 	zoom_range=0.15,
@@ -73,8 +73,8 @@ aug = ImageDataGenerator(
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
+# construir a cabeça do modelo que será colocada em cima do
+# o modelo básico
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -82,22 +82,22 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# coloque o modelo FC da cabeça em cima do modelo base (isso se tornará
+# o modelo real que vamos treinar)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+# faz um loop sobre todas as camadas no modelo base e as congela para que elas
+# *não* ser atualizado durante o primeiro processo de treinamento
 for layer in baseModel.layers:
 	layer.trainable = False
 
-# compile our model
+# compilar nosso modelo
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the head of the network
+# treinar o chefe da rede
 print("[INFO] training head...")
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
@@ -106,23 +106,23 @@ H = model.fit(
 	validation_steps=len(testX) // BS,
 	epochs=EPOCHS)
 
-# make predictions on the testing set
+# fazer previsões sobre o conjunto de testes
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+# para cada imagem no conjunto de teste, precisamos encontrar o índice do
+# rótulo com a maior probabilidade prevista correspondente
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
+# mostra um relatório de classificação bem formatado
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 
-# serialize the model to disk
+# serializar o modelo para o disco
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
 
-# plot the training loss and accuracy
+# plota a perda e a precisão do treinamento
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
